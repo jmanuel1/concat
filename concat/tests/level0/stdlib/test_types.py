141d22babd1bb649b4a4380e66687aca4298d418
Jason Manuel <jama.indo@hotmail.com>
2019-12-31 18:49:16 -0700

    Test that quotations compare equal to equivalent lists

--- concat/tests/level0/stdlib/test_types.py
+++ concat/tests/level0/stdlib/test_types.py
@@ -8,6 +8,11 @@ class TestQuotations(unittest.TestCase):
     def __push(self, value: object) -> Callable[[List[object], List[object]], None]:
         return lambda stack, _: stack.append(value)
 
+    def test_quoted_list_is_equal_to_list(self) -> None:
+        lst = [1, 2, 3]
+        quote = concat.level0.stdlib.types.Quotation(lst)
+        self.assertEqual(quote, lst, msg='Quotation([1, 2, 3]) != [1, 2, 3]')
+
     def test_quotation_is_callable(self) -> None:
         quote = concat.level0.stdlib.types.Quotation([self.__push('c')])
         stack = []
