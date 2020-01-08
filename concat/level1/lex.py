4b1ffb630bfff860d07d753865e6d13c96b876d9
Jason Manuel <jama.indo@hotmail.com>
2020-01-07 18:10:15 -0700

    Add level 1 lexer
    The lexer recognizes NONE tokens (the None keyword).

---
+++ concat/level1/lex.py
@@ -0,0 +1,35 @@
+"""The Concat lexer."""
+import sys
+from typing import Optional
+import concat.level0.lex
+
+
+__all__ = ['Lexer', 'lexer']
+
+
+class Lexer:
+    """Lexes the input given at initialization.
+
+    Use token() to get the next token.
+    """
+
+    def __init__(self):
+        self.__level_0_lexer = concat.level0.lex.lexer
+
+    def input(self, data: str) -> None:
+        """Initialize the Lexer object with the data to tokenize."""
+        self.__level_0_lexer.input(data)
+
+    def token(self) -> Optional[concat.level0.lex.Token]:
+        """Return the next token as a Token object."""
+        return self.__level_0_lexer.token()
+
+
+lexer = Lexer()
+
+if __name__ == '__main__':
+    lexer.input(sys.stdin.read())
+    token_ = lexer.token()
+    while token_ is not None:
+        print(repr(token_))
+        token_ = lexer.token()
---
+++ concat/tests/level1/test_lex.py
@@ -0,0 +1,43 @@
+import concat.level1.lex as lex
+import concat.level0.lex
+import unittest
+from typing import Tuple
+
+
+TokenTuple = Tuple[str, str, Tuple[int, int], Tuple[int, int]]
+
+
+class TestSmallExamples(unittest.TestCase):
+    examples = {
+        'None\n': (  # newline is important
+            ('ENCODING', 'utf-8', (0, 0), (0, 0)),
+            ('NONE', 'None', (1, 0), (1, 4)),
+            ('NEWLINE', '\n', (1, 4), (1, 5)),
+            ('ENDMARKER', '', (2, 0), (2, 0))
+        )
+    }
+
+    def test_examples(self) -> None:
+        for example in type(self).examples:
+            with self.subTest(example=example):
+                tokens = []
+                lex.lexer.input(example)
+                while True:
+                    token = lex.lexer.token()
+                    if token is None:
+                        break
+                    tokens.append(token)
+
+                expectationPairs = zip(tokens, type(self).examples[example])
+                self.assertTrue(
+                    all(map(self._matches_token, expectationPairs)))
+
+    def _matches_token(self, pair: Tuple[concat.level0.lex.Token, TokenTuple]) -> bool:
+        token, tokTuple = pair
+
+        return (
+            token.type == tokTuple[0] and
+            token.value == tokTuple[1] and
+            token.start == tokTuple[2] and
+            token.end == tokTuple[3]
+        )
