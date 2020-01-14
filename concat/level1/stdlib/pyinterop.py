3bc5fa1d99cad754974b3223b0f491c1648cb115
Jason Manuel <jama.indo@hotmail.com>
2020-01-13 17:51:32 -0700

    Add to_complex. to_slice, to_str as level 1 python interop functions
    They are factories for the complex, slice, and str types.

--- concat/level1/stdlib/pyinterop.py
+++ concat/level1/stdlib/pyinterop.py
@@ -21,3 +21,26 @@ def to_bool(stack: List[object], stash: List[object]) -> None:
 def to_float(stack: List[object], stash: List[object]) -> None:
     """x -- float(x)"""
     stack.append(float(stack.pop()))  # type: ignore
+
+
+def to_complex(stack: List[object], stash: List[object]) -> None:
+    """imag real -- complex(real, imag)"""
+    real, imag = stack.pop(), stack.pop()
+    if imag is None:
+        stack.append(complex(real))  # type: ignore
+    else:
+        stack.append(complex(real, imag))  # type: ignore
+
+
+def to_slice(stack: List[object], stash: List[object]) -> None:
+    """step stop start -- slice(start, stop, step)"""
+    stack.append(slice(stack.pop(), stack.pop(), stack.pop()))
+
+
+def to_str(stack: List[object], stash: List[object]) -> None:
+    """errors encoding object -- str(object, encoding, errors)"""
+    object, encoding, errors = (stack.pop() for _ in range(3))
+    if encoding is None and errors is None:
+        stack.append(str(object))
+    else:
+        stack.append(str(object, encoding, errors))  # type: ignore
--- concat/tests/level1/stdlib/test_python_concat_interface.py
+++ concat/tests/level1/stdlib/test_python_concat_interface.py
@@ -31,5 +31,33 @@ class TestObjectFactories(unittest.TestCase):
         stash: List[object] = []
         concat.level1.stdlib.pyinterop.to_float(stack, stash)
         message = 'to_float has incorrect stack effect'
-        self.assertIsInstance(stack[0], float, msg='to_float does not push a float')
+        self.assertIsInstance(
+            stack[0], float, msg='to_float does not push a float')
         self.assertAlmostEqual(cast(float, stack[0]), 10.0, 0, msg=message)
+
+    def test_to_complex(self) -> None:
+        """Test that to_complex works."""
+        stack: List[object] = [10, 20]
+        stash: List[object] = []
+        concat.level1.stdlib.pyinterop.to_complex(stack, stash)
+        message = 'to_complex has incorrect stack effect'
+        self.assertIsInstance(
+            stack[0], complex, msg='to_complex does not push a complex')
+        self.assertAlmostEqual(  # type: ignore
+            stack[0], 20 + 10j, 0, msg=message)
+
+    def test_to_slice(self) -> None:
+        """Test that to_slice works."""
+        stack: List[object] = [10, 20, 5]
+        stash: List[object] = []
+        concat.level1.stdlib.pyinterop.to_slice(stack, stash)
+        message = 'to_slice has incorrect stack effect'
+        self.assertEqual(stack, [slice(5, 20, 10)], msg=message)
+
+    def test_to_str(self) -> None:
+        """Test that to_str works."""
+        stack: List[object] = [None, None, 5]
+        stash: List[object] = []
+        concat.level1.stdlib.pyinterop.to_str(stack, stash)
+        message = 'to_str has incorrect stack effect'
+        self.assertEqual(stack, ['5'], msg=message)
