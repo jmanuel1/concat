193064b6a6fa70c329bc09d9d7e16a5d21358093
Jason Manuel <jama.indo@hotmail.com>
2020-03-21 19:13:52 -0700

    Add shuffle words to level 1 stdlib
    We borrow shuffle words from Factor and RetroForth. However, we do ot borrow Factors deprecated complex shuffle words.

---
+++ concat/level1/stdlib/shuffle_words.py
@@ -0,0 +1,71 @@
+"""Shuffle words are based on those of Factor and RetroForth.
+
+We do not borrow Factor's deprecated 'complex' shuffle words."""
+
+
+from typing import List
+
+
+def drop(stack: List[object], stash: List[object]) -> None:
+    """x -- """
+    stack.pop()
+
+
+def drop_2(stack: List[object], stash: List[object]) -> None:
+    """x y -- """
+    stack.pop()
+    stack.pop()
+
+
+def drop_3(stack: List[object], stash: List[object]) -> None:
+    """x y z -- """
+    stack.pop()
+    stack.pop()
+    stack.pop()
+
+
+def nip(stack: List[object], stash: List[object]) -> None:
+    """x y -- y"""
+    stack.pop(-2)
+
+
+def nip_2(stack: List[object], stash: List[object]) -> None:
+    """x y z -- z"""
+    stack.pop(-2)
+    stack.pop(-2)
+
+
+def dup(stack: List[object], stash: List[object]) -> None:
+    """x -- x x"""
+    stack.append(stack[-1])
+
+
+def dup_2(stack: List[object], stash: List[object]) -> None:
+    """x y -- x y x y"""
+    stack.append(stack[-2])
+    stack.append(stack[-2])
+
+
+def swap(stack: List[object], stash: List[object]) -> None:
+    """x y -- y x"""
+    stack[-2], stack[-1] = stack[-1], stack[-2]
+
+
+def dup_3(stack: List[object], stash: List[object]) -> None:
+    """x y z -- x y z x y z"""
+    stack += [stack[-3], stack[-2], stack[-1]]
+
+
+def over(stack: List[object], stash: List[object]) -> None:
+    """x y -- x y x"""
+    stack += [stack[-2]]
+
+
+def over_2(stack: List[object], stash: List[object]) -> None:
+    """x y z -- x y z x y"""
+    stack += [stack[-3], stack[-2]]
+
+
+def pick(stack: List[object], stash: List[object]) -> None:
+    """x y z -- x y z x"""
+    stack += [stack[-3]]
