9f059c7f3b0427b547e9fcf70c177124b91012df
Jason Manuel <jama.indo@hotmail.com>
2020-04-01 15:07:42 -0700

    Create a context manager to mock sys.stdin during certain tests

--- concat/tests/level1/stdlib/test_repl.py
+++ concat/tests/level1/stdlib/test_repl.py
@@ -1,21 +1,30 @@
 import unittest
 import io
 import sys
+import contextlib
 import concat.level1.stdlib.types
 import concat.level1.stdlib.repl
+from typing import TextIO, Iterator
+
+
+@contextlib.contextmanager
+def replace_stdin(input_stream: TextIO) -> Iterator[None]:
+    # don't use sys.__stdin__ because sys.stdin might not be the original one
+    original_stdin = sys.stdin
+    sys.stdin = input_stream
+    try:
+        yield
+    finally:
+        sys.stdin = original_stdin
 
 
 class TestREPLFunctions(unittest.TestCase):
     def test_read_quot(self):
-        original_stdin = sys.stdin
-        sys.stdin = io.StringIO('some words here')
         stack = []
         # Like in Factor, read_quot will search its caller's scope for objects.
         some, words, here = object(), object(), object()
-        try:
+        with replace_stdin(io.StringIO('some words here')):
             concat.level1.stdlib.repl.read_quot(stack, [])
-        finally:
-            sys.stdin = original_stdin
         self.assertEqual(
             stack,
             [concat.level1.stdlib.types.Quotation([some, words, here])],
