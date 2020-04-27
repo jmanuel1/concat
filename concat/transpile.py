0e865f02c3289dcf2379aff150a134e5935877a7
Jason Manuel <jama.indo@hotmail.com>
2020-04-26 18:14:12 -0700

    Put names from the preamble into the type environment

--- concat/transpile.py
+++ concat/transpile.py
@@ -5,6 +5,7 @@ import concat.level0.transpile
 import concat.level1.parse
 import concat.level1.transpile
 import concat.level1.typecheck
+import concat.level2.preamble_types
 import concat.level2.parse
 import concat.level2.typecheck
 from typing import cast
@@ -18,10 +19,13 @@ def transpile(code: str) -> ast.Module:
     parser.extend_with(concat.level2.typecheck.typecheck_extension)
     parser.extend_with(concat.level2.parse.level_2_extension)
     concat_ast = parser.parse(tokens)
-    # TODO: put names from the preamble into the type environment
     # FIXME: Consider the type of everything entered interactively beforehand.
+    environment = concat.level1.typecheck.Environment(
+        concat.level2.preamble_types.types)
     concat.level1.typecheck.infer(
-        concat.level1.typecheck.Environment(), concat_ast.children)
+        environment,
+        concat_ast.children,
+    )
     transpiler = concat.level0.transpile.VisitorDict[concat.level0.parse.Node,
                                                      ast.AST]()
     transpiler.extend_with(concat.level0.transpile.level_0_extension)
