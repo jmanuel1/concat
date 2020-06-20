9beab0da638ae71bfb2903f9edf9d3f537a89962
Jason Manuel <jama.indo@hotmail.com>
2020-06-20 12:57:22 -0700

    Remove junk from an older attempt at generators (level 1)

--- concat/level1/stdlib/types.py
+++ concat/level1/stdlib/types.py
@@ -23,74 +23,21 @@ Prior art:
 * https://softwareengineering.stackexchange.com/a/332383/153281
 """
 
-from typing import List, NoReturn, Generator, Optional, Callable, cast
+from typing import List, Generator
 import concat.level0.stdlib.types
 
 _ConcatGenerator = Generator[object, object, None]
-
-
-class _YieldException(Exception):
-    def __init__(self, continuation: Callable[[List[object], List[object]], None]) -> None:
-        self.continuation = continuation
 
 
 class Quotation(concat.level0.stdlib.types.Quotation):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self._is_generator_function = False
 
     def __call__(
         self, stack: List[object], stash: List[object]
-    ) -> Optional[_ConcatGenerator]:
-        if self._is_generator_function:
-            # Copy the stack and stash since they could change between each
-            # resumption.
-            generator = self._call_as_generator(stack[:], stash[:])
-            # push generator onto stack as a return value
-            stack.append(generator)
-            return generator
+    ) -> None:
         return super().__call__(stack, stash)
-
-    @staticmethod
-    def yield_function(stack: List[object], __: List[object]) -> NoReturn:
-        raise _YieldException(
-            cast(Callable[[List[object], List[object]], None], stack.pop()))
-
-    def _call_as_generator(
-        self, stack: List[object], stash: List[object]
-    ) -> _ConcatGenerator:
-        """Execute the quotation like a Python generator.
-
-        Note that control is returned to the next element of a quotation after
-        a yield. That means that in the code `(42 yield unreachable) reached`,
-        the word `unreachable` is never executed.
-        """
-        # done = False
-        # while not done:
-        #     super().__call__(stack, stash)
-        #     next_func = stack.pop()
-        #     value = stack.pop()
-        #     yield value
-        #     if next_func is None:
-        #         done = True
-        #     else:
-        raise NotImplementedError
-
-    def make_generator_function(self, stack: List[object], stash: List[object]) -> 'Quotation':
-        copy = Quotation(self)
-        try:
-            yield_index = copy.index(Quotation.yield_function)
-        except ValueError:
-            pass
-        else:
-            continuation = Quotation(copy[yield_index +
-                                          1:]).make_generator_function(stack, stash)
-            copy[yield_index + 1:] = []
-            copy[-1:-1] = [lambda s, t: s.append(continuation)]
-        copy._is_generator_function = True
-        stack.append(copy)
-        return copy
 
 
 def to_py_iter(stack: List[object], stash: List[object]) -> None:
