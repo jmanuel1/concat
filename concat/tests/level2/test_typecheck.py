4b7c19b798c22f45d8ea857577b78efd057a5f78
Jason Manuel <jama.indo@hotmail.com>
2020-06-22 22:46:33 -0700

    Add property-based level-2 type checker tests
    These test that the type inference algorithm works on a whole program and that the individual type unifier understands subtyping.

--- concat/tests/level2/test_typecheck.py
+++ concat/tests/level2/test_typecheck.py
@@ -7,7 +7,77 @@ import concat.level2.parse
 import concat.astutils
 import unittest
 from typing import List, Iterable, Type, Tuple, Dict, cast
+from hypothesis.strategies import (SearchStrategy, text, lists, builds,  # type: ignore
+                                   from_type, one_of, register_type_strategy,
+                                   just, integers, iterables,
+                                   binary)
+from hypothesis import given, infer  # type: ignore
 import parsy
+
+
+def register_builds_strategy(
+    type: Type[object],
+    *strategies: SearchStrategy,
+    **k_strategies: SearchStrategy
+) -> None:
+    strategy = builds(type, *strategies, **k_strategies)
+    register_type_strategy(type, strategy)
+
+# Strategy for generating type-correct programs
+
+
+del_statement_node = builds(concat.level1.parse.DelStatementNode,
+                            lists(from_type(concat.level0.parse.WordNode),
+                                  min_size=1))
+register_type_strategy(concat.level1.parse.DelStatementNode,
+                       del_statement_node)
+
+register_type_strategy(Iterable[concat.level0.parse.WordNode], iterables(
+    from_type(concat.level0.parse.WordNode)))
+
+register_type_strategy(Iterable[concat.astutils.Words], iterables(
+    from_type(concat.astutils.Words)))
+
+register_type_strategy(Iterable[Iterable[concat.level0.parse.WordNode]], iterables(
+    from_type(Iterable[concat.level0.parse.WordNode])))
+
+register_type_strategy(Iterable[Iterable[List[concat.level0.parse.WordNode]]], iterables(
+    from_type(Iterable[List[concat.level0.parse.WordNode]])))
+
+number_word_node = builds(concat.level0.parse.NumberWordNode,
+                          builds(concat.level0.lex.Token, text(), integers().map(repr), start=infer, end=infer))
+register_type_strategy(concat.level0.parse.NumberWordNode, number_word_node)
+
+string_word_node = builds(concat.level0.parse.StringWordNode,
+                          builds(concat.level0.lex.Token, text(), text().map(repr), start=infer, end=infer))
+register_type_strategy(concat.level0.parse.StringWordNode, string_word_node)
+
+slice_word_node = builds(concat.level1.parse.SliceWordNode,
+                         iterables(from_type(Iterable[concat.level0.parse.WordNode]), min_size=3, max_size=3))
+register_type_strategy(concat.level1.parse.SliceWordNode, slice_word_node)
+
+bytes_word_node = builds(concat.level1.parse.BytesWordNode,
+                         builds(concat.level0.lex.Token, text(), binary().map(repr), start=infer, end=infer))
+register_type_strategy(concat.level1.parse.BytesWordNode, bytes_word_node)
+
+register_builds_strategy(concat.level1.parse.DictWordNode, iterables(iterables(
+    from_type(concat.astutils.Words), min_size=2, max_size=2)), location=infer)
+
+attr_type_node = builds(concat.level2.typecheck.AttributeTypeNode,
+                        location=infer, name=text(), type=from_type(concat.level2.typecheck.IndividualTypeNode))
+register_type_strategy(
+    concat.level2.typecheck.AttributeTypeNode, attr_type_node)
+
+named_type_node = builds(concat.level2.typecheck.NamedTypeNode,
+                         location=infer, name=one_of(just('int'), just('bool'), just('object')))
+register_type_strategy(
+    concat.level2.typecheck.NamedTypeNode, named_type_node)
+
+typed_program = lists(one_of(builds(concat.level2.parse.CastWordNode), from_type(
+    concat.level1.parse.SimpleValueWordNode), from_type(concat.level0.parse.StatementNode))).filter(lambda x: len(x) == 0 or not isinstance(x[0], concat.level2.parse.CastWordNode))
+
+# print(from_type(concat.level0.parse.WordNode))
+# print(from_type(Iterable[concat.level0.parse.WordNode]))
 
 
 def lex_string(string: str) -> List[concat.level0.lex.Token]:
@@ -31,6 +101,18 @@ def build_parsers() -> concat.level0.parse.ParserDict:
 
 
 class TestTypeChecker(unittest.TestCase):
+
+    @unittest.skip('fails on del statements')
+    @given(typed_program)
+    def test_whole_program(self, program: concat.astutils.WordsOrStatements) -> None:
+        concat.level1.typecheck.infer(concat.level1.typecheck.Environment(
+        ), program, (concat.level2.typecheck.infer,), True)
+
+    @given(from_type(Tuple[concat.level1.typecheck.IndividualType, concat.level1.typecheck.IndividualType]).filter(lambda ts: ts[0].is_subtype_of(ts[1])))
+    def test_unify_ind(self, types: Tuple[concat.level1.typecheck.IndividualType, concat.level1.typecheck.IndividualType]) -> None:
+        type_1, type_2 = types
+        concat.level1.typecheck.unify_ind(type_1, type_2)
+
     def test_function_with_stack_effect(self) -> None:
         funcdef = 'def f(a b -- c): ()\n'
         tree = parse(funcdef)
