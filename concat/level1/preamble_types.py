096f07bdebe0a829996d0d92b18c0361fa9a7d54
Jason Manuel <jama.indo@hotmail.com>
2021-04-06 17:08:04 -0700

    Universally quantify the types of 'if_then' and 'call'
    By the way, we can successfully type-check 'if_then'. There is no test yet
    for 'call'.

--- concat/level1/preamble_types.py
+++ concat/level1/preamble_types.py
@@ -3,6 +3,7 @@ from concat.level1.typecheck.types import (
     SequenceVariable,
     ForAll,
     StackEffect,
+    ObjectType,
     bool_type,
     dict_type,
     file_type,
@@ -21,14 +22,14 @@ _stack_var = SequenceVariable()
 _a_var = IndividualVariable()
 _b_var = IndividualVariable()
 _c_var = IndividualVariable()
+_x = IndividualVariable()
 
 types = {
-    # FIXME: Types should be universally quantified.
-    'if_then': StackEffect(
+    'if_then': ObjectType(_x, {'__call__': StackEffect(
         [_rest_var, bool_type, StackEffect([_rest_var], [_rest_var])],
         [_rest_var],
-    ),
-    'call': StackEffect(
+    )}, [_rest_var]),
+    'call': ObjectType(_x, {'__call__': StackEffect(
         [_rest_var, StackEffect([_rest_var], [_seq_var])], [_seq_var],
-    ),
+    )}, [_rest_var, _seq_var]),
 }
