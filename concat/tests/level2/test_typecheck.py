2619bbd4d9ace78e00d0ffc712d915e4df3da41e
Jason Manuel <jama.indo@hotmail.com>
2021-06-20 13:33:08 -0700

    Reenable `test_{function_with_strict_effect,pushed_subscription}` (level 2)
    These tests used to be too slow to run; they are no longer. In particular,
    the runtime of `test_function_with_strict_effect` has decreased from 69
    seconds to 35 seconds on my machine, and `test_pushed_subscription` runs in
    less than a second on my machine.

--- concat/tests/level2/test_typecheck.py
+++ concat/tests/level2/test_typecheck.py
@@ -60,7 +60,6 @@ class TestTypeChecker(unittest.TestCase):
             True,
         )
 
-    @unittest.skip('TOO SLOW')
     def test_function_with_strict_effect(self) -> None:
         """Test that a function type checks with a strict annotated effect.
 
@@ -107,7 +106,6 @@ class TestTypeChecker(unittest.TestCase):
         )
 
     # QUESTION: Should fail?
-    @unittest.skip('TOO SLOW')
     def test_pushed_subscription(self) -> None:
         """Test that the type checker allows pushed subscription words."""
         tree = parse('$[0] cast (int) 1 +')
