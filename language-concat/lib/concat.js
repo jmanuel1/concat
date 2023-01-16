1f79927c6ca7ed1c1692d81fc33624ca99a674a1
Jason Manuel <jama.indo@hotmail.com>
2023-01-16 16:05:24 -0700

    Interact with the Concat process asynchronously
    This is great for the responsiveness of typing, but now there's a
    noticeable "flash of unhighlighted text" when you finish typing. I think
    this can be mitigated by destroying the old markers as late as possible.

--- language-concat/lib/concat.js
+++ language-concat/lib/concat.js
@@ -1,18 +1,21 @@
-const { execFileSync } = require("child_process");
-const { existsSync } = require("fs");
+const { execFile: nodeExecFile } = require("child_process");
+const { exists: nodeExists } = require("fs");
 const { join } = require("path");
+const { promisify } = require("util");
+
+const execFile = promisify(nodeExecFile);
+const exists = promisify(nodeExists);
 
 module.exports = class Concat {
-  constructor() {
-    this._remainingTokens = [];
-  }
-
-  tokenize(line) {
-    const stdout = execFileSync(this.findPython(), ["-m", "concat", "--tokenize"], {
+  async * tokenize(line, editor) {
+    const promise = execFile(await this.findPython(editor), ["-m", "concat", "--tokenize"], {
       encoding: "utf-8",
-      input: line,
       windowsHide: true
     });
+    const process = promise.child;
+    process.stdin.write(line);
+    process.stdin.end();
+    const { stdout } = await promise;
     const tokens = JSON.parse(stdout);
     const remainingTokens = tokens.map(token => {
       return {
@@ -24,32 +27,20 @@ module.exports = class Concat {
       };
     });
 
-    let finished = false;
-
-    return () => {
-      if (remainingTokens.length > 0) {
-        const token = remainingTokens.shift();
-        if (remainingTokens.length === 0) {
-          finished = true;
-        }
-        return token;
-      }
-      if (finished) {
-        throw new Error('no more tokens');
-      }
-    };
+    for (const token of remainingTokens) {
+      yield token;
+    }
   }
 
-  findPython() {
+  async findPython(editor) {
     const virtualEnvPaths = ["env/Scripts/python.exe", "env/bin/python"];
-    const editor = atom.workspace.getActiveTextEditor();
-    if (!editor) return "python";
+    if (!editor) throw new Error("You need a text editor to find the path to Python.");
     const editorPath = editor.getPath();
     const [projectPath, _] = atom.project.relativizePath(editorPath);
     if (!projectPath) return "python";
     for (let path of virtualEnvPaths) {
       path = join(projectPath, path);
-      if (existsSync(path)) {
+      if (await exists(path)) {
         return path;
       }
     }
---
+++ language-concat/lib/javascript-semantic-grammar.ts
@@ -0,0 +1,195 @@
+// Concat Highlighting Package for Atom
+// Copyright (c) 2022-2023 Jason Manuel <jama.indo@hotmail.com>
+//
+// Based on JavaScript Semantic Highlighting Package for Atom
+// Copyright (c) 2014-2015 Philipp Emanuel Weidmann <pew@worldwidemann.com>
+//
+// Released under the terms of the MIT License (http://opensource.org/licenses/MIT)
+let JavaScriptSemanticGrammar;
+const $ = require("jquery");
+import {
+  Disposable,
+  Grammar,
+  GrammarRegistry,
+  DisplayMarker,
+  TextBuffer,
+  TextEditor,
+} from "atom";
+// TODO: Use a dummy textmate grammar instead.
+const Grammar = Object.getPrototypeOf(
+  Object.getPrototypeOf(atom.grammars.getGrammars()[0])
+).constructor as new (registry: any, options: any) => Grammar;
+const Concat = require("./concat.js");
+
+module.exports =
+  // QUESTION: Can I just implement the Grammar interface instead?
+  JavaScriptSemanticGrammar = class JavaScriptSemanticGrammar extends Grammar {
+    private concat: typeof Concat;
+    private registry: GrammarRegistry;
+    private textEditorsObserver?: Disposable;
+
+    constructor(registry) {
+      const name = "Concat";
+      const scopeName = "source.concat";
+      super(registry, { name, scopeName, fileTypes: ["cat"] });
+      this.concat = new Concat();
+      this.registry = registry.textmateRegistry;
+      this.startMarkingTokens();
+    }
+
+    destroy() {
+      this.textEditorsObserver?.dispose();
+    }
+
+    async acornTokenize(line: string, editor: TextEditor) {
+      const tokens = [];
+
+      const tokenizer = this.concat.tokenize(line, editor);
+      for await (let token of tokenizer) {
+        if (token.type.type === "eof") {
+          break;
+        }
+        tokens.push(token);
+      }
+      return { tokens };
+    }
+
+    tokenScopes(token, text) {
+      if (token.type.type === "NAME") {
+        if (
+          [
+            "None",
+            "True",
+            "False",
+            "Ellipsis",
+            "...",
+            "NotImplemented",
+          ].includes(token.value)
+        ) {
+          return ["identifier", "constant"];
+        }
+        return ["identifier"];
+      } else if (token.type.type === "COMMENT") {
+        return ["comment"];
+      } else if (token.isKeyword) {
+        return ["keyword"];
+      } else if (token.type.type === "NUMBER") {
+        return ["constant", "numeric"];
+      } else if (token.type.type === "STRING") {
+        const singleQuoteIndex = token.value.indexOf("'");
+        const doubleQuoteIndex = token.value.indexOf('"');
+        let firstQuoteIndex: number;
+        if (singleQuoteIndex < 0)
+          firstQuoteIndex = doubleQuoteIndex;
+        else if (doubleQuoteIndex < 0) {
+          firstQuoteIndex = singleQuoteIndex;
+        } else {
+          firstQuoteIndex = Math.min(singleQuoteIndex, doubleQuoteIndex);
+        }
+        if (/r/i.test(token.value.slice(0, firstQuoteIndex))) {
+          return ["string", "regexp"];
+        }
+        return ["string"];
+      }
+      return null;
+    }
+
+    tokenizeLine(line, ruleStack, firstLine) {
+      if (firstLine == null) {
+        firstLine = false;
+      }
+      const tags = [];
+      const tokens = [];
+      return { line, tags, tokens, ruleStack: [] };
+    }
+
+    startMarkingTokens() {
+      const concatTextEditors = new Map();
+      this.textEditorsObserver = atom.workspace.observeTextEditors((editor) => {
+        let destroyObserver;
+        const grammarObserver = editor.observeGrammar((grammar) => {
+          if (grammar === this) {
+            const state: {
+              markers: DisplayMarker[];
+              buffer: TextBuffer;
+              editor: TextEditor;
+              changeObserver?: Disposable;
+            } = { markers: [], buffer: editor.getBuffer(), editor };
+            // initial highlight
+            this.markTokens(null, state);
+            state.changeObserver = editor
+              .getBuffer()
+              .onDidStopChanging((event) => {
+                this.markTokens(event.changes, state);
+              });
+            concatTextEditors.set(editor, state);
+          } else {
+            concatTextEditors.get(editor)?.changeObserver?.dispose();
+            concatTextEditors
+              .get(editor)
+              ?.markers?.forEach((marker) => marker.destroy());
+            concatTextEditors.delete(editor);
+          }
+        });
+        destroyObserver = editor.onDidDestroy(function () {
+          grammarObserver.dispose();
+          destroyObserver.dispose();
+        });
+      });
+    }
+
+    markTokens(changes, state) {
+      return this.markTokensForChange(changes, state);
+    }
+
+    async markTokensForChange(change, state) {
+      let range;
+      state.markers.forEach((marker) => marker.destroy());
+      state.markers = [];
+      let text = state.editor.getText();
+
+      const tokens = [];
+
+      const addToken = (text, range, scopes = null) =>
+        tokens.push({ value: text, scopes, range });
+
+      const tokenizeResult = await this.acornTokenize(text, state.editor);
+      const acornTokens = tokenizeResult.tokens;
+
+      let tokenPos = 0;
+      for (let token of acornTokens) {
+        console.log(token);
+        text = token.value;
+        const tokenScopes = this.tokenScopes(token, text);
+        console.log(tokenScopes);
+        range = [
+          [token.start[0] - 1, token.start[1]],
+          [token.end[0] - 1, token.end[1]],
+        ];
+        console.log(token.start, token.end, range);
+        if (tokenScopes != null) {
+          addToken(text, range, tokenScopes);
+          tokenPos = token.end;
+        }
+      }
+
+      const result = [];
+      for (let processedToken of tokens) {
+        // we destroy the markers ourselves
+        var marker = state.buffer.markRange(processedToken.range, {
+          invalidate: "never",
+        });
+        state.markers.push(marker);
+        result.push(
+          processedToken.scopes.forEach(function (scope) {
+            const cssClass = "syntax--" + scope;
+            state.editor.decorateMarker(marker, {
+              type: "text",
+              class: cssClass,
+            });
+          })
+        );
+      }
+      return result;
+    }
+  };
