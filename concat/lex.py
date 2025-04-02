0c4e6eda591ec7b2c248305613342f0f1dbdf56a
jmanuel1 <7255867+jmanuel1@users.noreply.github.com>
2025-04-02 05:53:35 +0000

    Fix issues found by linters

--- concat/lex.py
+++ concat/lex.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
-from concat.astutils import Location, are_on_same_line_and_offset_by
+
 import dataclasses
 import io
 import json
-import tokenize as py_tokenize
 import token
-from typing import Iterator, List, Literal, Optional, Tuple, Union
+import tokenize as py_tokenize
+from typing import (
+    Iterator,
+    List,
+    Literal,
+    Optional,
+    Tuple,
+    Union,
+)
+
+from concat.astutils import Location, are_on_same_line_and_offset_by
 
 
 @dataclasses.dataclass
