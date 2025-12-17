29070155687f5f4a207a6c909b2f50334d4dede2
Jason Manuel <jama.indo@hotmail.com>
2025-12-17 00:39:14 +0000

    Try all possible python paths when launching language server

--- language-concat/lib/lsp-client.ts
+++ language-concat/lib/lsp-client.ts
@@ -1,5 +1,8 @@
-import { AutoLanguageClient } from "atom-languageclient";
+import { AutoLanguageClient, LanguageServerProcess } from "atom-languageclient";
 import Concat from "./concat";
+import { execFile as nodeExecFile, ExecFileOptions } from "child_process";
+import { isErrnoException } from "./error";
+import { promisify } from "util";
 
 /**
  * A Language Server Protocol client for Concat.
@@ -22,18 +25,57 @@ class ConcatLanguageClient extends AutoLanguageClient {
   }
 
   override startServerProcess(projectPath: string) {
-    atom.config.set("core.debugLSP", true);
-    return super.spawn(
-      Concat.collectPossiblePythonPaths(projectPath)[0],
-      ["-m", "concat.lsp"],
-      { cwd: projectPath, windowsHide: true }
+    const possiblePythonPaths = Concat.collectPossiblePythonPaths(projectPath);
+    return this.startServerProcessUsingFoundPython(
+      possiblePythonPaths,
+      projectPath,
     );
+  }
+
+  private startServerProcessUsingFoundPython(
+    pythonPaths: string[],
+    projectPath: string,
+  ) {
+    if (pythonPaths.length === 0) {
+      throw Error("Could not find Python executable");
+    }
+
+    const process = super.spawn(pythonPaths[0], ["-m", "concat.lsp"], {
+      cwd: projectPath,
+      windowsHide: true,
+    });
+    return new Promise<LanguageServerProcess>((resolve, reject) => {
+      let alreadyResolved = false;
+      process.on("error", (error) => {
+        if (alreadyResolved) {
+          console.error(error);
+          return;
+        }
+
+        if (isErrnoException(error) && error.code === "ENOENT") {
+          resolve(
+            this.startServerProcessUsingFoundPython(
+              pythonPaths.slice(1),
+              projectPath,
+            ),
+          );
+        } else {
+          reject(error);
+        }
+        alreadyResolved = true;
+      });
+      /* https://nodejs.org/docs/latest-v14.x/api/child_process.html#child_process_subprocess_pid */
+      if (process.pid !== undefined) {
+        resolve(process);
+        alreadyResolved = true;
+      }
+    });
   }
 
   public override restartAllServers() {
     this.logger.debug("restarting Concat server");
     return super.restartAllServers();
-  };
+  }
 }
 
 export default new ConcatLanguageClient();
